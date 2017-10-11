bc642aa2b7a43c1525636505b92d2cf29b6e75dd
DESKTOP-J5HSKQ2\USER <nplasencia82@gmail.com>
2017-10-11 02:46:58 -0500

    Rutas en ExpressJS

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 'use strict';
-
+var express =  require('express'); 
+var server = express();
 var port = process.env.PORT || 8000; 
 
-var http = require('http');
-
-var server = http.createServer(function(req,res){
-	res.writeHead(200, {'content_type': 'text/plain'});
-	res.end("Hello World")
-});
-
 server.listen(port, function(req,res) {
-	console.log('El servidor esta escuchando en el puerto '+ port)
+	console.log('El servidor esta escuchando en el puerto '+ port);
 }); 
+
+server.get('/',function(req,res){
+	res.send('Hello World');
+});
