72433da12777a6ae313e750a851ed95c6014ff1a
DESKTOP-J5HSKQ2\USER <nplasencia82@gmail.com>
2017-10-11 02:30:29 -0500

    Variables de Entorno

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 'use strict';
+
+var port = process.env.PORT || 8000; 
+
 var http = require('http');
 
 var server = http.createServer(function(req,res){
 	console.log("Se esta haciendo una peticion")
 });
 
-server.listen(8000, function(req,res) {
-	console.log('El servidor esta corriendo en el puerto '+ 8000)
-});
-
-server.on('request', function (req,res)  {
-	console.log('Estoy haciendo una peticion');
-});
+server.listen(port, function(req,res) {
+	console.log('El servidor esta escuchando en el puerto '+ port)
+}); 
