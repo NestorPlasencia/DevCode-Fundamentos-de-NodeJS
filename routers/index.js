c6a844c567cc89bccdd0abff76b2d6451bd1507e
NestorPlasencia <plasencia_2007@hotmail.com>
2017-10-13 20:16:33 -0500

    agregar aplicacion user

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,9 @@
 'use strict';
 var router_main = require('../apps/main/controllers.js');
+var router_user = require('../apps/user/controllers.js');
+
 var routers = function(server) {
-	server.use('/', router_main)
+	server.use('/', router_main);
+	server.use('/', router_user);
 };
 module.exports = routers;
