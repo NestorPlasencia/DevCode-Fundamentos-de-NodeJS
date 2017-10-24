c0dc2fdd77493c98988a830cb96812ed621fef3b
NestorPlasencia <plasencia_2007@hotmail.com>
2017-10-24 00:14:19 -0500

    visualizacion de usuario en cosola luego de auth

--- apps/main/controllers.js
+++ apps/main/controllers.js
@@ -3,6 +3,7 @@ var express = require('express');
 var router = express.Router();
 router.route('/')
 	.get(function(req,res){
+		console.log(req.user);
 		res.render('main/index.html')
 	});
 module.exports = router;
